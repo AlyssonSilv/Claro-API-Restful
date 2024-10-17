a4c513480f260c96aff26c6f7fc6c3f584a2d75f
AlyssonSilv <alyssonsamueldasilva6@gmail.com>
2024-10-17 10:30:43 -0300

    Initial commit

---
+++ src/test/java/me/dio/Claro_API_Restfull/ClaroApiRestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.Claro_API_Restfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaroApiRestfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
